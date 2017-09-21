1b975983b2c6ae3f221ed5b6cd625b42110413e3
Aryan Soni <aryansoni1108@gmail.com>
2017-09-22 00:31:34 +0530

    pause shake added

--- app/src/main/java/com/sample/andremion/musicplayer/activities/DetailActivity.java
+++ app/src/main/java/com/sample/andremion/musicplayer/activities/DetailActivity.java
@@ -16,9 +16,20 @@
 
 package com.sample.andremion.musicplayer.activities;
 
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.design.widget.FloatingActionButton;
 import android.transition.Transition;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.andremion.music.MusicCoverView;
 import com.sample.andremion.musicplayer.R;
@@ -27,11 +38,22 @@ import com.sample.andremion.musicplayer.view.TransitionAdapter;
 public class DetailActivity extends PlayerActivity {
 
     private MusicCoverView mCoverView;
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private ShakeDetector mShakeDetector;
+    private CameraManager manager;
+    Intent mServiceIntent;
+    FloatingActionButton discfab;
+    Context ctx;
+    public Context getCtx(){
+        return ctx;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_detail);
+        discfab = (FloatingActionButton) findViewById(R.id.fab);
 
         mCoverView = (MusicCoverView) findViewById(R.id.cover);
         mCoverView.setCallbacks(new MusicCoverView.Callbacks() {
@@ -53,6 +75,30 @@ public class DetailActivity extends PlayerActivity {
                 mCoverView.start();
             }
         });
+        ctx = this;
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager
+                .getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mShakeDetector = new ShakeDetector();
+        mShakeDetector.setOnShakeListener(new ShakeDetector.OnShakeListener() {
+
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void onShake(int count) {
+				/*
+				 * The following method, "handleShakeEvent(count):" is a stub //
+				 * method you would use to setup whatever you want done once the
+				 * device has been shook.**/
+
+                if(count==3){
+                    Toast.makeText(DetailActivity.this, "Hello", Toast.LENGTH_SHORT).show();
+                    onFabClick(discfab);
+                }
+
+
+            }
+        });
+
     }
 
     @Override
@@ -63,6 +109,52 @@ public class DetailActivity extends PlayerActivity {
     public void onFabClick(View view) {
         pause();
         mCoverView.stop();
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Add the following line to register the Session Manager Listener onResume
+        mSensorManager.registerListener(mShakeDetector, mAccelerometer, SensorManager.SENSOR_DELAY_UI);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        // Add the following line to unregister the Sensor Manager onPause
+        mSensorManager.unregisterListener(mShakeDetector);
+        // TODO: Consider calling
+        //    ActivityCompat#requestPermissions
+        // here to request the missing permissions, and then overriding
+        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+        //                                          int[] grantResults)
+        // to handle the case where the user grants the permission. See the documentation
+        // for ActivityCompat#requestPermissions for more details.
+        return;
+    }
+
+
+
+
+
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+
+    }
+
+
+    private boolean isMyServiceRunning(Class<?> serviceClass){
+        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningAppProcessInfo service : manager.getRunningAppProcesses()){
+            if(serviceClass.getName().equals(service.getClass())){
+                Log.d("isMyservicerunning?",true+"");
+                return true;
+            }
+        }
+        Log.d("isMyServiceRunning?",false+"");
+        return false;
     }
 
 }
--- app/src/main/java/com/sample/andremion/musicplayer/activities/MainActivity.java
+++ app/src/main/java/com/sample/andremion/musicplayer/activities/MainActivity.java
@@ -1,34 +1,32 @@
-/*
- * Copyright (c) 2016. André Mion
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 
 package com.sample.andremion.musicplayer.activities;
-
+import android.app.ActivityManager;
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v4.util.Pair;
 import android.support.v4.view.ViewCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.sample.andremion.musicplayer.R;
 import com.sample.andremion.musicplayer.music.MusicContent;
 import com.sample.andremion.musicplayer.view.RecyclerViewAdapter;
+
+import static com.sample.andremion.musicplayer.R.id.options;
 
 public class MainActivity extends PlayerActivity {
 
@@ -38,11 +36,25 @@ public class MainActivity extends PlayerActivity {
     private View mDurationView;
     private View mProgressView;
     private View mFabView;
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private ShakeDetector mShakeDetector;
+    private CameraManager manager;
+    private boolean isFlashOn = false;
+    Intent mServiceIntent;
+    Context ctx;
+    public Context getCtx(){
+        return ctx;
+    }
+    FloatingActionButton fab;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_list);
+
 
         //
         mCoverView = findViewById(R.id.cover);
@@ -57,6 +69,31 @@ public class MainActivity extends PlayerActivity {
         assert recyclerView != null;
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(new RecyclerViewAdapter(MusicContent.ITEMS));
+        ctx = this;
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager
+                .getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mShakeDetector = new ShakeDetector();
+        mShakeDetector.setOnShakeListener(new ShakeDetector.OnShakeListener() {
+
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void onShake(int count) {
+				/*
+				 * The following method, "handleShakeEvent(count):" is a stub //
+				 * method you would use to setup whatever you want done once the
+				 * device has been shook.**/
+
+                if(count==2){
+                    Toast.makeText(MainActivity.this, "Hello", Toast.LENGTH_SHORT).show();
+                    onFabClick(mFabView);
+                }
+
+
+            }
+        });
+
+
     }
 
     public void onFabClick(View view) {
@@ -70,5 +107,56 @@ public class MainActivity extends PlayerActivity {
                 new Pair<>(mFabView, ViewCompat.getTransitionName(mFabView)));
         ActivityCompat.startActivity(this, new Intent(this, DetailActivity.class), options.toBundle());
     }
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Add the following line to register the Session Manager Listener onResume
+        mSensorManager.registerListener(mShakeDetector, mAccelerometer, SensorManager.SENSOR_DELAY_UI);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        // Add the following line to unregister the Sensor Manager onPause
+        mSensorManager.unregisterListener(mShakeDetector);
+        // TODO: Consider calling
+        //    ActivityCompat#requestPermissions
+        // here to request the missing permissions, and then overriding
+        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+        //                                          int[] grantResults)
+        // to handle the case where the user grants the permission. See the documentation
+        // for ActivityCompat#requestPermissions for more details.
+        return;
+    }
+
+
+
+
+
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+
+    }
+
+
+    private boolean isMyServiceRunning(Class<?> serviceClass){
+        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningAppProcessInfo service : manager.getRunningAppProcesses()){
+            if(serviceClass.getName().equals(service.getClass())){
+                Log.d("isMyservicerunning?",true+"");
+                return true;
+            }
+        }
+        Log.d("isMyServiceRunning?",false+"");
+        return false;
+    }
+
 
 }
+
+
+
+
